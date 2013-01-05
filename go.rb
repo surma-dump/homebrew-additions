7746d1a46df3fd8b6e2d35356fe6cd64a0f356c7
Alexander Surma <surma@surmair.de>
2013-01-05 22:57:39 +0100

    Add formula for from-soruce Go

---
+++ go.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class Go < Formula
+  homepage 'http://golang.org'
+  url 'https://code.google.com/p/go', :using => :hg, :branch => 'release'
+  version '1.0.3'
+
+  def install
+    cp_r Dir["#{downloader.cached_location}/*", "#{downloader.cached_location}/.hg*"], "#{prefix}"
+    ENV['GOROOT'] = "#{prefix}"
+    Dir.chdir "#{prefix}/src" do
+      system "./all.bash"
+    end
+  end
+
+  def caveats; <<-EOS.undent
+    Add these to your shell profile:
+      export GOROOT="#{prefix}"
+    EOS
+  end
+end
